42dd8c097617c3faff463f662e6d96a1fef31fed
VGAHOME <VGAHOME@192.168.0.7>
2014-07-26 17:02:50 -0300

    Theme

---
+++ webapp/src/main/java/ar/com/vga/action/HomeAction.java
@@ -0,0 +1,14 @@
+package ar.com.vga.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HomeAction extends ActionSupport {
+
+	private static final long serialVersionUID = 4899761917248012629L;
+
+	public String prepareShow(){
+		
+		return SUCCESS;
+	}
+	
+}
